83ea33fe75d0ac0fb46caf5d61e41d5ad114156a
Alejandro Mancebo <Antarebo@gmail.com>
2017-10-23 21:29:15 -0300

    Init2.0

---
+++ mantelArch/mantelArch-ejb/src/main/java/com/Mantel/mantelArch/model/Administrador.java
@@ -0,0 +1,8 @@
+package com.Mantel.mantelArch.model;
+
+public class Administrador extends Usuario {
+
+	public Administrador(String username, String password, String email, String direccion, String telefono, String nombre, String apellido) {
+		super(username, password, email, direccion, telefono);
+	}
+}
